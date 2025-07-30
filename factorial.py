f9f9078bbddb3f1f1d369bda01589a8267a6c37e
Nikita-1432011 <steppers.nikita3159@gmail.com>
2025-07-30 20:58:56 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,15 @@
+def factorial(n):
+
+    if (n==0 or n==1):
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+num = int(input("Enter a number"))
+    
+if num < 0:
+        print("Factorial does not exist for nagative numbers.")
+
+else:
+
+        print(f"the factorial of {num} is {factorial(num)}")
